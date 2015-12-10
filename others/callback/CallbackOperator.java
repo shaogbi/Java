26edc61dff0912dd7354492d11c9d681c6f37a58
shaogbi <bishaogang@gmail.com>
2015-12-09 23:19:34 -0800

    Update CallbackOperator.java

--- others/callback/CallbackOperator.java
+++ others/callback/CallbackOperator.java
@@ -1,8 +1,14 @@
 package others.callback;
 
 public class CallbackOperator {
-  public static void executeCallback(ICallback callback) {
-    System.out.println("Following will execute callback:");
-    callback.executeCallback();
-  }
+    public static void executeCallback(ICallback callback) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Following will execute callback:");
+                callback.executeCallback();
+            }
+        }).start();
+        System.out.println("After calling callback, let's wait for callback's result...");
+    }
 }
