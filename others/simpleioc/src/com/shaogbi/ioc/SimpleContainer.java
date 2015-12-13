4e269b9d6762c5e939eb460f39fee8d18425ba52
shaogbi <bishaogang@gmail.com>
2015-12-13 00:43:31 -0800

    Update SimpleContainer.java

--- others/simpleioc/src/com/shaogbi/ioc/SimpleContainer.java
+++ others/simpleioc/src/com/shaogbi/ioc/SimpleContainer.java
@@ -1,8 +1,9 @@
 package com.shaogbi.ioc;
 
 import java.lang.reflect.Field;
+import java.util.Collections;
 import java.util.Map;
-import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.shaogbi.ioc.annotation.SimpleAutowired;
@@ -17,11 +18,17 @@ import com.shaogbi.ioc.util.ReflectUtil;
  */
 public class SimpleContainer {
     /**
-     * key is class name, value is singleton instance.
+     * Store bean names which are registered.
+     */
+    private Set<String> registeredBeans;
+    /**
+     * Key is class name, value is singleton instance.
+     * This map store and cache registered beans, along with their dependencies.
      */
     private Map<String, Object> beans;
     
     public SimpleContainer() {
+        registeredBeans = Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>());
         beans = new ConcurrentHashMap<String, Object>();
     }
     
@@ -32,46 +39,58 @@ public class SimpleContainer {
     @SuppressWarnings("unchecked")
     public <T> T getBean(Class<T> clazz) {
         String name = clazz.getName();
-        Object bean = beans.get(name);
-        return Optional.ofNullable((T) bean).orElse(null);
+        if(registeredBeans.contains(name)) {
+            return (T) beans.get(name);
+        } else {
+            return null;
+        }
     }
     
     /**
      * @param clazz, class name
      */
-    public void registerBean(Class<?> clazz) {
-        if(clazz.isAnnotationPresent(SimpleBean.class)) {
-            String name = clazz.getName();
-            recursiveAssembleBean(name);
+    public <T> void registerBean(Class<T> clazz) {
+        if(clazz.isInterface()) {
+            throw new RuntimeException("Register interface is not allowed!");
+        }
+        if(!clazz.isAnnotationPresent(SimpleBean.class)) {
+            throw new RuntimeException("Registered bean must be with @SimpleBean annotation!");
+        }
+        String name = clazz.getName();
+        if(!registeredBeans.contains(name)) {
+            try {
+                recursiveAssembleBean(name);
+                registeredBeans.add(name);
+            } catch (Exception e) {
+                System.err.print("Register bean failed: ");
+                e.printStackTrace();
+            }
         }
     }
     
     /**
      * @param name, class name with full path.
      * @return Object
+     * @throws Exception 
      */
-    private Object recursiveAssembleBean(String name) {
+    private Object recursiveAssembleBean(String name) throws Exception {
         Object rawBean = ReflectUtil.newInstance(name);
         return recursiveAssembleBean(name, rawBean);
     }
     
-    // TODO: how to deal with primitive type like String, Integer
-    private Object recursiveAssembleBean(String name, Object object) {
+    // TODO: how to handle primitive type like String, Integer; how to handle beans with name
+    private Object recursiveAssembleBean(String name, Object object) throws Exception {
         Field[] fields = object.getClass().getDeclaredFields();
         for(Field field: fields) {
             if(field.isAnnotationPresent(SimpleAutowired.class)) {
-                // create or get autowired bean
+                // create, or get autowired bean from cache
                 String className = field.getType().getCanonicalName();
                 Object autowiredObj = beans.getOrDefault(className, recursiveAssembleBean(className));
                 
                 // set attribute for this field
                 boolean accessible = field.isAccessible();
                 field.setAccessible(true);
-                try {
-                    field.set(object, autowiredObj);
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
+                field.set(object, autowiredObj);
                 field.setAccessible(accessible);
             }
         }
