2cc72e5ca0f26edd2a8c42709d045cd3f6cf1889
shaogbi <bishaogang@gmail.com>
2015-12-13 00:44:31 -0800

    Update TestSimpleContainer.java

--- others/simpleioc/tst/TestSimpleContainer.java
+++ others/simpleioc/tst/TestSimpleContainer.java
@@ -1,4 +1,6 @@
 import com.shaogbi.ioc.SimpleContainer;
+import com.shaogbi.ioc.teststuff.BookDao;
+import com.shaogbi.ioc.teststuff.ISampleInterface;
 import com.shaogbi.ioc.teststuff.SelfreportRunner;
 
 public class TestSimpleContainer {
@@ -11,5 +13,10 @@ public class TestSimpleContainer {
         SelfreportRunner selfreportRunner2 = container.getBean(SelfreportRunner.class);
         System.out.println(selfreportRunner.hashCode());
         System.out.println(selfreportRunner2.hashCode()); // these 2 hash code are same because of singleton
+        
+        BookDao bookDao = container.getBean(BookDao.class);
+        System.out.println("BookDao is " + bookDao); // should be null because it is not registered
+        
+        container.registerBean(ISampleInterface.class);
     }
 }
